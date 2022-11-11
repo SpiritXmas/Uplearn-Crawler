78ab7a2494a2bf5d95946bd9a12373022c7a7073
Adam <52005747+SpiritXmas@users.noreply.github.com>
2022-11-11 22:27:22 +0000

    moved old version to XP only selenium and added post only

--- main.py
+++ main.py
@@ -1,186 +1,88 @@
-from math import ceil
-from time import sleep
+import requests
+from json import loads
+from re import sub
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.chrome.options import Options
+Endpoints = {
+  "validateQuestion":"https://app.uplearn.co.uk/validateQuizNew.php",
+  "validateQuiz":"https://app.uplearn.co.uk/validateSectionQuiz.php"
+}
 
-chrome_options = Options()
+OK = requests.codes.ok
+SESSION = requests.Session()
 
-chrome_options.add_experimental_option('excludeSwitches', ['enable-logging', 'disable-popup-blocking', 'enable-automation'])
-chrome_options.add_experimental_option('useAutomationExtension', False)
+AUTH_TOKEN = ""
+PHP_SES_ID = "" # bruh idk, its accepting my static one and the one I grab normally doesn't work
+USER_ID = ""
 
-chrome_options.add_argument('log-level=3')
-chrome_options.add_argument('--start-maximized')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_argument("--incognito")
-chrome_options.add_argument("--disable-popup-blocking")
-chrome_options.add_argument("disable-infobars")
-chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+email = input("Enter email: ")
+password = input("Enter password: ")
 
-chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36")
+json_data={'operationName':'AuthenticateUserByEmail','variables':{'email':email,'password':password},'query':'mutation AuthenticateUserByEmail($email: String!, $password: String!) {\n  authenticationResult: authenticateUserByEmail(\n    email: $email\n    password: $password\n  ) {\n    token\n    user {\n      id\n      name\n      email\n      phoneNumber\n      signedUpAt\n      gender\n      roles\n      parentId\n      studentId\n      lastLoggedInIp\n      utmSource\n      utmMedium\n      utmCampaign\n      intercomHash\n      isMerckUser\n      emailVerified\n      emailBounced\n      multipleSignUps\n      accountSetupCompleted\n      introductionSource\n      postcode\n      notificationPreferences {\n        allowAllEmails\n        allowCourseNotifications\n        allowReminderEmails\n        __typename\n      }\n      schoolYear\n      subjectRequests {\n        id\n        board {\n          id\n          __typename\n        }\n        subject {\n          id\n          __typename\n        }\n        qualification {\n          id\n          __typename\n        }\n        __typename\n      }\n      school {\n        id\n        name\n        __typename\n      }\n      managedSchool {\n        id\n        name\n        __typename\n      }\n      enrolments {\n        id\n        score\n        moduleId\n        isTrial\n        xp\n        enrolmentPlan\n        renewedAt\n        createdAt\n        __typename\n      }\n      childEnrolments {\n        id\n        __typename\n      }\n      schoolEnrolments {\n        id\n        optInStatus\n        school {\n          id\n          name\n          __typename\n        }\n        __typename\n      }\n      purchases {\n        id\n        isTrial\n        trialType\n        __typename\n      }\n      onboardingSession {\n        id\n        currentStep\n        studentGoal\n        questionsInStageOne\n        courseId\n        selectedCourse {\n          id\n          title\n          uniqueCode\n          __typename\n        }\n        chooseCourseStepOptions {\n          id\n          displayName\n          course {\n            id\n            title\n            uniqueCode\n            __typename\n          }\n          __typename\n        }\n        chooseSubsectionStepOptions {\n          id\n          displayName\n          subsection {\n            id\n            name\n            __typename\n          }\n          __typename\n        }\n        subsectionId\n        selectedSubsection {\n          id\n          name\n          uniqueCode\n          subsectionQuizAvailable\n          __typename\n        }\n        endedAt\n        __typename\n      }\n      experimentMemberships {\n        id\n        experiment {\n          id\n          uniqueCode\n          isActive\n          __typename\n        }\n        status\n        startedAt\n        __typename\n      }\n      experimentGroups {\n        id\n        experiment {\n          id\n          name\n          description\n          uniqueCode\n          expiryDate\n          isActive\n          __typename\n        }\n        activities(filter: {order: 1}) {\n          id\n          order\n          activity {\n            id\n            uniqueCode\n            __typename\n          }\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}'}
 
-driver = webdriver.Chrome(options=chrome_options)
+with SESSION as http:
+  data = http.post('https://web.uplearn.co.uk/api/', json=json_data)
+  if data.status_code == OK:
+    parsed = None
 
-
-driver.get("https://web.uplearn.co.uk/login")
-
-input("Press enter once logged in...")
-
-xp_wanted = ceil(int(input("Desired xp gain: "))/16)
-
-delay = 0.2
-
-
-#main class
-
-def ProtectedCall(function):
     try:
-        function()
-    except: pass
+      parsed = loads(data.text)
+    except:
+      quit("Error with parsing login data.")
 
-    return True
+    if parsed["data"]["authenticationResult"] == None:
+      quit("Incorrect password or email!")
 
-class Search:
-    def Button(text, click=False):
-        btn, timeout = False, 0
+    AUTH_TOKEN = parsed["data"]["authenticationResult"]["token"]
+    USER_ID = parsed["data"]["authenticationResult"]["user"]["id"]
+  else:
+    quit("Error with POST request to login.")
 
-        while not btn:
-            try:
-                if not click:
-                    btn = driver.find_elements(By.XPATH, '//button[normalize-space()="' + text + '"]')
-                else:
-                    btn = driver.find_element(By.XPATH, '//button[normalize-space()="' + text + '"]')
-            except:
-                sleep(0.1)
-                timeout += 1
-
-            if timeout >= 100:
-                raise Exception("Timeout within function 'Button' with arguments text:'" + text + "' and click:'" + str(click) + "'")
-        
-        if not click:
-            return btn
-        
-        btn.click()
-
-        return True
-    
-    def CheckBox(option, click=False, option2=""):
-        chckbx, timeout = False, 0
-
-        search_string = ""
-        if option2 == "":
-            search_string = "//input[@value='" + option + "']"
-        else:
-            search_string = "//input[@value='" + option + "'][@name='" + option2 + "']"
-
-        while not chckbx:
-            try:
-                if not click:
-                    chckbx = driver.find_elements(By.XPATH, search_string)
-                else:
-                    chckbx = driver.find_element(By.XPATH, search_string) 
-            except:
-                sleep(0.1)
-                timeout += 1
-
-            if timeout >= 100:
-                raise Exception("Timeout within function 'CheckBox' with arguments option:'" + option + "' and click:'" + str(click) + "' and option2:'" + option2 + "'")
-
-        if not click:
-            return chckbx
-        
-        chckbx.click()
-
-        return True
-
-    def DropDown(name, value):
-        drpdown, timeout = False, 0
-
-        while not drpdown:
-            try:
-                drpdown = driver.find_element(By.XPATH, "//select[@name='" + name + "']")
-            except:
-                sleep(0.1)
-                timeout += 1
-
-            if timeout >= 100:
-                raise Exception("Timeout within function 'DropDown' with arguments name:'" + name + "' and value:'" + value + "'")
-        
-        select = Select(drpdown)
-
-        select.select_by_value(value)
-
-    def IntermediateNextPage():
-        Search.Button("I know it", True)
-        Search.Button("Submit", True)
-        Search.Button("Continue ->", True)
-
-    def NextPage():
-        timeout = 0
-        while not ProtectedCall(Search.IntermediateNextPage):
-            timeout += 1
-            sleep(0.1)
-
-            if timeout >= 100:
-                return Exception("Timeout within function 'NextPage' with null arguments.")
+  data = http.get('https://app.uplearn.co.uk/validateSectionQuiz.php')
+  if data.status_code == OK:
+    PHP_SES_ID = data.headers["Set-Cookie"].split(";")[0]
+  else:
+    quit("Error with GET request to grab php session id.")
 
 
-for iteration in range(xp_wanted):
-    driver.get("https://app.uplearn.co.uk/learn/maths-1/symbols-and-meaning/subsection-quiz")
+print("")
 
-    # initialise
 
-    Search.Button("Begin?", True)
+def Post(url, data):
+  data = sub("user=?(.*?)&m", "user=" + USER_ID + "&m", data)
+  request = requests.post(url, data, headers={"cookie":"PHPSESSID=1e4545b76368c6210487d66d12724fb5; auth-token=" + AUTH_TOKEN, "content-type": "application/x-www-form-urlencoded; charset=UTF-8"})
+  
+  if request.status_code == OK:
+    return request
 
-    sleep(delay)
+  return False
 
-    # sifting through questions
+AnswerPaths = {
+  "FoundationsI":{
+    "Number":["optionsCheckboxes[0][]=option1&optionsCheckboxes[0][]=option2&optionsCheckboxes[0][]=option3&optionsCheckboxes[0][]=option4&quizType[0][]=multimultiplechoice&quizValidate[0][]=148&quizID=148&user=1&module=30&format=s&currentLesson=463&timeOnCur=0&confidenceValue=3", "optionsRadios[0][]=option1&quizType[0][]=multiplechoice&quizValidate[0][]=149&quizID=149&user=1&module=30&format=s&currentLesson=463&timeOnCur=0&confidenceValue=3", "ended=1&user=1&module=30&section=463"],
 
-    Search.CheckBox("option2", True)
-    Search.CheckBox("option4", True)
-    Search.NextPage()
+    "SymbolsAndMeaning":["optionsCheckboxes[0][]=option2&optionsCheckboxes[0][]=option4&quizType[0][]=multimultiplechoice&quizValidate[0][]=150&quizID=150&user=1&module=30&format=s&currentLesson=464&timeOnCur=0&confidenceValue=3", "optionsCheckboxes[0][]=option2&quizType[0][]=multimultiplechoice&quizValidate[0][]=151&quizID=151&user=1&module=30&format=s&currentLesson=464&timeOnCur=0&confidenceValue=3", "optionsRadios[0][]=option3&quizType[0][]=multiplechoice&quizValidate[0][]=152&optionsRadios[1][]=option1&quizType[1][]=multiplechoice&quizValidate[1][]=152&quizID=152&user=1&module=30&format=s&currentLesson=464&timeOnCur=0&confidenceValue=3", "optionsCheckboxes[0][]=option1&optionsCheckboxes[0][]=option2&optionsCheckboxes[0][]=option3&optionsCheckboxes[0][]=option4&quizType[0][]=multimultiplechoice&quizValidate[0][]=153&quizID=153&user=1&module=30&format=s&currentLesson=464&timeOnCur=0&confidenceValue=3", "optionsCheckboxes[0][]=option2&optionsCheckboxes[0][]=option4&quizType[0][]=multimultiplechoice&quizValidate[0][]=154&quizID=154&user=1&module=30&format=s&currentLesson=464&timeOnCur=0&confidenceValue=3", "optionsCheckboxes[0][]=option1&optionsCheckboxes[0][]=option2&optionsCheckboxes[0][]=option3&optionsCheckboxes[0][]=option5&optionsCheckboxes[0][]=option7&optionsCheckboxes[0][]=option8&quizType[0][]=multimultiplechoice&quizValidate[0][]=155&quizID=155&user=1&module=30&format=s&currentLesson=464&timeOnCur=0&confidenceValue=3", "optionsDropdown[0][]=%E2%89%A1&quizType[0][]=dropdown&quizValidate[0][]=156&optionsDropdown[1][]=%E2%89%A1&quizType[1][]=dropdown&quizValidate[1][]=156&optionsDropdown[2][]=%3D&quizType[2][]=dropdown&quizValidate[2][]=156&optionsDropdown[3][]=%E2%89%A1&quizType[3][]=dropdown&quizValidate[3][]=156&optionsCheckboxes[4][]=option1&optionsCheckboxes[4][]=option3&quizType[4][]=multimultiplechoice&quizValidate[4][]=156&quizID=156&user=1&module=30&format=s&currentLesson=464&timeOnCur=0&confidenceValue=3", "optionsCheckboxes[0][]=option1&quizType[0][]=multimultiplechoice&quizValidate[0][]=157&quizID=157&user=1&module=30&format=s&currentLesson=464&timeOnCur=0&confidenceValue=3", "ended=1&user=1&module=30&section=464"],
 
-    Search.CheckBox("option2", True)
-    Search.NextPage()
+    "Arithmetic":[]
+                 }
+}
 
-    Search.CheckBox("option3", True, "optionsRadios[0][]")
-    Search.CheckBox("option1", True, "optionsRadios[1][]")
-    Search.NextPage()
+Parent, Child = AnswerPaths[input("Enter a category: ")], ""
+if Parent:
+  Child = Parent[input("Enter a quiz: ")]
 
-    Search.CheckBox("option1", True)
-    Search.CheckBox("option2", True)
-    Search.CheckBox("option3", True)
-    Search.CheckBox("option4", True)
-    Search.NextPage()
+  if not Child:
+    quit()
+else:
+  quit()
 
-    Search.CheckBox("option2", True)
-    Search.CheckBox("option4", True)
-    Search.NextPage()
+Length = len(Child)-1
+for index, value in enumerate(Child):
+  if index != Length:
+    Post(Endpoints["validateQuestion"], value)
+    continue
 
-    Search.CheckBox("option1", True)
-    Search.CheckBox("option2", True)
-    Search.CheckBox("option3", True)
-    Search.CheckBox("option5", True)
-    Search.CheckBox("option7", True)
-    Search.CheckBox("option8", True)
-    Search.NextPage()
+  Post(Endpoints["validateQuiz"], value)
 
-    Search.DropDown("optionsDropdown[0][]", "≡")
-    Search.DropDown("optionsDropdown[1][]", "≡")
-    Search.DropDown("optionsDropdown[2][]", "=")
-    Search.DropDown("optionsDropdown[3][]", "≡")
-    Search.CheckBox("option1", True)
-    Search.CheckBox("option3", True)
-    Search.NextPage()
+#result = Post(Endpoints["validateQuiz"], data="ended=1&user=1&module=30&section=464")
 
-    Search.CheckBox("option1", True)
-    Search.NextPage()
-
-    sleep(1)
-
-driver.quit()
-
-print("Finished xp gain.")
+#print(result.text)
